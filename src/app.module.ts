dfac12b8c7d72c7a0ae852e1f13703c30d241e45
longtruong2604 <longtruong2604@gmail.com>
2025-10-27 00:39:15 +0700

    chore: add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,42 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { validateEnv } from './config/env';
+import { Env, validateEnv } from './config/env';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, validate: validateEnv })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService<Env, true>) => {
+        const nodeEnv = config.get('NODE_ENV', { infer: true });
+        const level = config.get('LOG_LEVEL', { infer: true });
+        return {
+          pinoHttp: {
+            level,
+            transport:
+              nodeEnv === 'development'
+                ? { target: 'pino-pretty', options: { singleLine: true } }
+                : undefined,
+
+            genReqId: (req) =>
+              (req.headers['x-request-id'] as string) || crypto.randomUUID(),
+            redact: {
+              paths: [
+                'req.headers.authorization',
+                'req.body.password',
+                'res.body.token',
+              ],
+              remove: true,
+            },
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 import type { Env } from './config/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   const config = app.get<ConfigService<Env, true>>(ConfigService);
-
-  const port = config.get('PORT', { infer: true }); // inferred as number
+  const port = config.get('PORT', { infer: true });
 
   await app.listen(port);
 }
