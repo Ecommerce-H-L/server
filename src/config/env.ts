9d8a24146a6f3174d200d2c19a380795cafb64b5
longtruong2604 <longtruong2604@gmail.com>
2025-10-27 00:26:55 +0700

    chore: add envConfig

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const EnvSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().int().positive().default(3000),
+  LOG_LEVEL: z
+    .enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace'])
+    .default('info'),
+});
+
+export type Env = z.infer<typeof EnvSchema>;
+export const validateEnv = (config: Record<string, unknown>) =>
+  EnvSchema.parse(config);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import type { Env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get<ConfigService<Env, true>>(ConfigService);
+
+  const port = config.get('PORT', { infer: true }); // inferred as number
+
+  await app.listen(port);
 }
 bootstrap();
