a864a680b5069a4452266668b27f53eaac92ac4a
longtruong2604 <longtruong2604@gmail.com>
2025-11-03 01:39:34 +0700

    feat: add prisma error utils

---
+++ src/utils/prisma-error.util.ts
@@ -0,0 +1,19 @@
+import { Prisma } from '@prisma/client';
+
+export function isUniqueConstraintPrismaError(
+  error: unknown,
+): error is Prisma.PrismaClientKnownRequestError {
+  return (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === 'P2002'
+  );
+}
+
+export function isNotFoundPrismaError(
+  error: unknown,
+): error is Prisma.PrismaClientKnownRequestError {
+  return (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === 'P2025'
+  );
+}
